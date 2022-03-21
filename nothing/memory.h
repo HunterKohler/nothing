5bf87413388120a101fccb3a1d662a51e1c25023
John Hunter Kohler <jhunterkohler@gmail.com>
2022-03-21 16:39:16 -0400

    update memory header

--- nothing/memory.h
+++ nothing/memory.h
@@ -12,44 +12,35 @@ namespace nothing {
  * Wrapper for a deleting function `Delete` to allow empty-base-class
  * optimization with `unique_ptr`.
  */
-template <class T, auto Delete>
+template <class T, std::invocable<T *> auto Delete>
 struct empty_deleter {
-    template <class U>
-    using enable_if_convertible =
-        std::enable_if_t<std::is_convertible_v<U *, T *>>;
-
-  public:
     constexpr empty_deleter() noexcept = default;
 
-    template <class U, enable_if_convertible<U> * = nullptr>
-    constexpr empty_deleter(const empty_deleter<U, Delete> &) noexcept
-    {}
+    template <class U>
+        requires std::convertible_to<U *, T *>
+    constexpr empty_deleter(const empty_deleter<U, Delete> &) noexcept {}
 
-    constexpr void operator()(T *ptr) noexcept(noexcept(Delete(ptr)))
+    constexpr void operator()(T *ptr) const noexcept(noexcept(Delete(ptr)))
     {
         Delete(ptr);
     }
 };
 
 /*
- * Partial specialization of `empty_deleter` for array types.
+ * Specialization of `empty_deleter` for array types.
  */
-template <class T, auto Delete>
+template <class T, std::invocable<T *> auto Delete>
 struct empty_deleter<T[], Delete> {
-    template <class U>
-    using enable_if_convertible =
-        std::enable_if_t<std::is_convertible_v<U (*)[], T (*)[]>>;
-
-  public:
     constexpr empty_deleter() noexcept = default;
 
-    template <class U, enable_if_convertible<U> * = nullptr>
-    constexpr empty_deleter(const empty_deleter<U, Delete> &d) noexcept
-    {}
+    template <class U>
+        requires std::convertible_to<U (*)[], T (*)[]>
+    constexpr empty_deleter(const empty_deleter<U[], Delete> &) noexcept {}
 
     template <class U>
-    constexpr enable_if_convertible<U>
-    operator()(U *ptr) noexcept(noexcept(Delete(ptr)))
+        requires std::convertible_to<U (*)[], T (*)[]> &&
+            std::invocable<decltype(Delete), U *>
+    constexpr void operator()(U *ptr) const noexcept(noexcept(Delete(ptr)))
     {
         Delete(ptr);
     }

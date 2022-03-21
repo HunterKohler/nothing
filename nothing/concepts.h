66692fc230b50d65248756ec72853e326a2518b6
John Hunter Kohler <jhunterkohler@gmail.com>
2022-03-21 16:38:19 -0400

    add type traits and concepts

--- nothing/concepts.h
+++ nothing/concepts.h
@@ -4,18 +4,42 @@
 #ifndef NOTHING_CONCEPTS_H_
 #define NOTHING_CONCEPTS_H_
 
-// Clang format struggles with C++20 Concepts.
-// clang-format off
-
-#include <type_traits>
 #include <concepts>
 #include <iterator>
+#include <nothing/type_traits.h>
+
+// clang-format off
 
 namespace nothing {
 
 template <class B>
 concept boolean_testable = std::convertible_to<B, bool> &&
-    requires(B&& b) { { !std::forward<B>(b) } -> std::convertible_to<bool>; };
+    requires(B &&b) { { !std::forward<B>(b) } -> std::convertible_to<bool>; };
+
+template <class T>
+concept arithmetic = std::is_arithmetic_v<T>;
+
+template <class T, class... Args>
+concept implicitly_constructible_from =
+    std::constructible_from<T, Args...> &&
+    (sizeof...(Args) == 0 && requires(T &t) { t = {}; }) &&
+    (sizeof...(Args) == 1 &&
+        requires(T &t, nth_element_t<0, Args...> &f) { T{f}; t = f; });
+
+template <class T, class U>
+concept narrowable_to =
+    requires(T t, U u) { static_cast<U>(t); static_cast<T>(u); } &&
+    ((!arithmetic<T> && !arithmetic<U>) ||
+     (arithmetic<T> && arithmetic<U> &&
+      std::totally_ordered<T> && std::totally_ordered<U> &&
+      std::is_default_constructible_v<T> &&
+      std::is_default_constructible_v<U>));
+
+template <class T>
+concept trivially_copyable = std::is_trivially_copyable_v<T>;
+
+template <class T, class U>
+concept explicitly_convertible_to = requires(T t) { static_cast<U>(t); };
 
 } // namespace nothing
 
--- nothing/type_traits.h
+++ nothing/type_traits.h
@@ -1,93 +1,39 @@
-/*
- * Copyright (C) 2021-2022 John Hunter Kohler <jhunterkohler@gmail.com>
- */
-#ifndef NOTHING_TYPE_TRAITS_H_
-#define NOTHING_TYPE_TRAITS_H_
-
 #include <type_traits>
 
 namespace nothing {
 
-template <class T, class... U>
-struct is_some : std::disjunction<std::is_same<T, U>...> {};
+template <std::size_t, class... Args>
+struct nth_element;
 
-template <class T>
-struct is_boolean_testable
-    : std::conjunction<
-          std::is_convertible<T, bool>,
-          std::is_convertible<decltype(!std::declval<T>()), bool>> {};
+template <std::size_t N>
+struct nth_element<N> {};
 
-template <class T>
-inline constexpr bool is_boolean_testable_v = is_boolean_testable<T>::value;
+template <class T, class... Args>
+struct nth_element<0, T, Args...> : std::type_identity<T> {};
+
+template <std::size_t N, class T, class... Args>
+struct nth_element<N, T, Args...> : nth_element<N - 1, Args...> {};
+
+template <std::size_t N, class... Args>
+using nth_element_t = typename nth_element<N, Args...>::type;
 
 namespace detail {
 
 template <class T, class U, class = void>
-struct is_weakly_equality_comparable_with : std::false_type {};
+struct is_explicitly_convertible_to_impl : std::false_type {};
 
 template <class T, class U>
-struct is_weakly_equality_comparable_with<
-    T, U,
-    std::enable_if_t<std::conjunction_v<
-        is_boolean_testable<
-            decltype(std::declval<const std::remove_reference_t<T> &>() ==
-                     std::declval<const std::remove_reference_t<U> &>())>,
-        is_boolean_testable<
-            decltype(std::declval<const std::remove_reference_t<T> &>() !=
-                     std::declval<const std::remove_reference_t<U> &>())>,
-        is_boolean_testable<
-            decltype(std::declval<const std::remove_reference_t<U> &>() ==
-                     std::declval<const std::remove_reference_t<T> &>())>,
-        is_boolean_testable<
-            decltype(std::declval<const std::remove_reference_t<U> &>() !=
-                     std::declval<const std::remove_reference_t<T> &>())>>>>
+struct is_explicitly_convertible_to_impl<
+    T, U, std::void_t<decltype(static_cast<U>(std::declval<T>()))>>
     : std::true_type {};
 
-}; // namespace detail
-
-template <class T>
-struct is_equality_comparable
-    : detail::is_weakly_equality_comparable_with<T, T> {};
-
-template <class T>
-inline constexpr bool is_equality_comparable_v =
-    is_equality_comparable<T>::value;
+} // namespace detail
 
 template <class T, class U>
-struct is_equality_comparable_with
-    : std::conjunction<is_equality_comparable<T>, is_equality_comparable<U>,
-                       detail::is_weakly_equality_comparable_with<T, U>
-                       // std::common_reference_with<
-                       // const std::remove_reference_t<T>&,
-                       // const std::remove_reference_t<U>&> &&
-                       //   is_equality_comparable<
-                       //     std::common_reference_t<
-                       //       const std::remove_reference_t<T>&,
-                       //       const std::remove_reference_t<U>&>
-                       //       >
-                       > {};
+struct is_explicitly_convertible_to
+    : detail::is_explicitly_convertible_to_impl<T, U> {};
 
 template <class T, class U>
-inline constexpr bool is_equality_comparable_with_v =
-    is_equality_comparable_with<T, U>::value;
-
-template <class T, class U>
-struct is_same_sign : std::conjunction<std::is_signed<T>, std::is_signed<U>> {
-    static_assert(std::is_arithmetic_v<T> && std::is_arithmetic_v<U>,
-                  "Types must be arithmetic to compare signedness");
-};
-
-template <class T, class U>
-inline constexpr bool is_same_sign_v = is_same_sign<T, U>::value;
-
-template <class T, class U>
-struct is_mutually_convertible
-    : std::conjunction<std::is_convertible<T, U>, std::is_convertible<U, T>> {};
-
-template <class T, class U>
-inline constexpr bool is_mutually_convertible_v =
-    is_mutually_convertible<T, U>::value;
-
+inline constexpr bool is_explicitly_convertible_to_v =
+    is_explicitly_convertible_to<T, U>::value;
 } // namespace nothing
-
-#endif

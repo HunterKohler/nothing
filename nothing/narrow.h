4acdbcf7d9577365366f7d4374648c63e6fcdfd5
John Hunter Kohler <jhunterkohler@gmail.com>
2022-03-21 16:39:26 -0400

    update narrow header

--- nothing/narrow.h
+++ nothing/narrow.h
@@ -4,21 +4,21 @@
 #ifndef NOTHING_NARROW_H_
 #define NOTHING_NARROW_H_
 
-#include <nothing/type_traits.h>
+#include <nothing/concepts.h>
 
 namespace nothing {
 
 class narrowing_error : public std::exception {
-    const char *what() const noexcept
-    {
-        return "narrowing error";
-    }
+    const char *what() const noexcept { return "narrowing error"; }
 };
 
+// clang-format off
+
 template <class T, class U>
-constexpr std::enable_if_t<!std::is_arithmetic_v<T>, T> narrow(U value)
+    requires (!arithmetic<T>) && narrowable_to<U, T>
+constexpr T narrow(U value)
 {
-    const auto result = static_cast<T>(value);
+    const auto result{ static_cast<T>(value) };
 
     if (static_cast<U>(result) != value) {
         throw narrowing_error{};
@@ -28,17 +28,21 @@ constexpr std::enable_if_t<!std::is_arithmetic_v<T>, T> narrow(U value)
 }
 
 template <class T, class U>
-constexpr std::enable_if_t<std::is_arithmetic_v<T>, T> narrow(U value)
+    requires arithmetic<T> && narrowable_to<U, T>
+constexpr T narrow(U value)
 {
-    const auto result = static_cast<T>(value);
+    const auto result{ static_cast<T>(value) };
 
     if (static_cast<U>(result) != value ||
-        (!is_same_sign_v<T, U> && ((value < U{}) != (result < T{})))) {
+        (std::is_signed_v<T> != std::is_signed_v<U> &&
+         ((value < U{}) != (result < T{})))) {
         throw narrowing_error{};
     }
 
     return result;
 }
+
+// clang-format on
 
 } // namespace nothing
 
